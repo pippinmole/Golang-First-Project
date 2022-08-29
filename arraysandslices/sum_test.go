6463b3d80c4d4f6627a4c429d38544028e1cd2c9
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2022-08-30 00:28:24 +0100

    Added SumAll with passing test
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-enough-code-to-make-it-pass-2

--- arraysandslices/sum_test.go
+++ arraysandslices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -24,4 +27,14 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d wanted %d given %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
