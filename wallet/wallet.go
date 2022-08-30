c819c64742a899fa9d3ed65a2d3cd83ba55763db
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2022-08-31 00:53:05 +0100

    Added withdraw, deposit and insufficient funds tests

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,13 +1,37 @@
 package wallet
 
-type Wallet struct {
-	balance int
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
