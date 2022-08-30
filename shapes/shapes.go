7737053ff89592827efb49b8e4a0e9865c334bbe
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2022-08-30 01:47:59 +0100

    Added triangle struct with passing tests

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -15,6 +15,15 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.Base * t.Height)
+}
+
 func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Height + r.Width)
 }
